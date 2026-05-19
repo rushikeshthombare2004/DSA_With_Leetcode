72a1d0af4aa31df38045c23f32a8729a39e0f2e5
Rushikesh Thombare <rushikeshthombare162004@gmail.com>
2026-05-19 21:05:01 +0530

    Time: 31 ms (53.62%) | Memory: 48.5 MB (67.03%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int candy : candyType) {
+            set.add(candy);
+        }
+
+        return Math.min(set.size(), candyType.length / 2);
+    }
+}

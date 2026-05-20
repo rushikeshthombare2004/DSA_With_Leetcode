acc87320620562c19a812ec79a975d9faf184f60
Rushikesh Thombare <rushikeshthombare162004@gmail.com>
2026-05-20 10:05:08 +0530

    Time: 2 ms (95.44%) | Memory: 47.7 MB (50.56%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int[] freq = new int[n + 1];
+        int duplicate = -1, missing = -1;
+
+        
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        
+        for (int i = 1; i <= n; i++) {
+            if (freq[i] == 2) duplicate = i;
+            if (freq[i] == 0) missing = i;
+        }
+
+        return new int[]{duplicate, missing};
+    }
+}
